26d23f3347665bad6b02875ab754627350031028
NOne null <nonenull@MacBook-Pro-NOne.local>
2023-09-13 23:37:19 +0200

    check element in page

--- test/test_main_page.py
+++ test/test_main_page.py
@@ -1,13 +1,21 @@
+
 import pytest
 import time
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 
 
 class Test_work:
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        browser.get(link)
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
